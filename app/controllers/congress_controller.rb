dbcda9aa392caf177b486a75d0dd841e8e81969a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-31 22:59:45 -0600

    complete webmock lesson

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -1,8 +1,14 @@
 class CongressController < ApplicationController
     def search
-      conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-        faraday.headers["X-API-KEY"] = ENV['govt_api_key']
-      end
+      # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      #   faraday.headers["X-API-KEY"] = ENV['govt_api_key']
+      # end
+
+      conn = Faraday.new(
+        url: "https://api.propublica.org", 
+        headers: { 'X-API-KEY' => ENV['govt_api_key'] }
+      )
+
       response = conn.get("/congress/v1/116/senate/members.json")
   
       data = JSON.parse(response.body, symbolize_names: true)
